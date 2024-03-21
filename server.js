54f6dfd15ca94bca270faecfc5934563ba34bace
abisdbest <105310709+abisdbest@users.noreply.github.com>
2024-03-21 21:05:52 +0000

    hi2

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cron = require('node-cron');
-const http = require('http');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,24 +9,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-// Schedule a cron job to ping the server every minute
-cron.schedule('* * * * *', () => {
-  console.log('Pinging server...');
-  http.get('https://blooket1-chat-server.onrender.com', (response) => {
-    let data = '';
-
-    // A chunk of data has been received
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received
-    response.on('end', () => {
-      console.log('Server pinged successfully.');
-    });
-  }).on('error', (err) => {
-    console.error('Error pinging server:', err);
-  });
 });
